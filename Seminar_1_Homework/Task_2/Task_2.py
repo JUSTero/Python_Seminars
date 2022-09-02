179b2ba1fdd12ddb84f6e7b107aff3763451b998
JUSTero <skorneta@mail.ru>
2022-09-02 22:32:17 +0300

    Task 2 complete

---
+++ Seminar_1_Homework/Task_2/Task_2.py
@@ -0,0 +1,15 @@
+﻿
+# Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+x = input(f'Enter x ')
+y = input(f'Enter y ')
+z = input(f'Enter z ')
+
+res1 = not(x or y or z)
+res2 = not x and not y and not z
+res = res1 == res2
+
+if res == True:
+    print('The statement is true')
+else:
+    print('The statement is false')
