43d2fab0b4980bdfe1aa66f6c2743e063ee316b6
JUSTero <skorneta@mail.ru>
2022-09-08 13:55:50 +0300

    Task 3 remake

--- Seminar_2/Task 3/Task_3.py
+++ Seminar_2/Task 3/Task_3.py
@@ -1,9 +1,8 @@
 # -*- coding: cp1251 -*-
 
 N = int(input('Введите количество членов '))
-num = 1
 
 for i in range(N):
-    print(f'{i + 1}. {num}')
-    num *= -3
+    print((-3) ** i, end = ' ')
+print()
 
