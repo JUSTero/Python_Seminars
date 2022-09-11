a422f2d91af0090f7ed71cd424dd09dfbfa9eb3b
JUSTero <skorneta@mail.ru>
2022-09-11 20:59:35 +0300

    Task 4 complete

--- Seminar_2_Homework/Task 4/Task_4.py
+++ Seminar_2_Homework/Task 4/Task_4.py
@@ -7,20 +7,19 @@ from random import randint
 
 
 N = int(input('Количество элементов = '))
-numbers = []
-numbers = [randint(-N, N + 1) for i in range(N)]
-data = []
-avr = 1
-
-
-
-with open('TextFile1.txt') as TF:
-    print(*TF)
-    data = [(int(line.strip()) for line in TF)]
-    print(data)
-for i in data:
-    avr = avr * numbers[i]
-    print(i)
-
+numbers = [randint(-N, N + 1) for i in range(N)]                # Заполнение списка случайными числами
+multipliers = [randint(0, N - 1) for i in range(4)]             # Заполнение списка множителей для записи в файл
+mult = []                                                       
 print(numbers)
+avr = 1
+with open('file.txt','w') as data:                              # Запись множителей в файл
+    for i in range(4):
+        data.writelines(f'{str(multipliers[i])}')
+with open('file.txt','r') as path:                              # Чтение сножителей из файла
+    for i in range(4):
+        mult.append(int(path.read(1)))                          # Запись множителей в список
+    print(mult)
+for i in mult:                                                  # Нахождение произведения чисел, стоящих на индексах множителей
+    avr *= numbers[i]
 print(avr)
+
