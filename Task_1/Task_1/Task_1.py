3e8289f340690b79ba73afa198b3013db7097944
JUSTero <skorneta@mail.ru>
2022-09-15 13:43:56 +0300

    Task 1 complete

---
+++ Task_1/Task_1/Task_1.py
@@ -0,0 +1,17 @@
+# -*- coding: cp1251 -*-
+#Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
+
+
+def sum(list):
+    sum = 0
+    for i in range(1, len(list), 2):
+        sum += list[i]
+    return sum
+
+
+numbers = []
+N = int(input("Количество чисел = "))
+for i in range(N):
+    numbers.append(float(input()))
+
+print("Сумма равна ", sum(numbers))
