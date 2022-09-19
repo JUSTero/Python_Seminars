bab3acbacce9231f4ad916d1bbc4ba258c918e6f
JUSTero <skorneta@mail.ru>
2022-09-19 12:35:01 +0300

    Task 5 complete

--- Seminar_3/Task_5/Task_5.py
+++ Seminar_3/Task_5/Task_5.py
@@ -1,2 +1,11 @@
 # -*- coding: cp1251 -*-
 # Реализуйте алгоритм задания случайных чисел без использования встроенного генератора псевдослучайных чисел.
+
+import random
+import time
+
+def getrand(x, y):
+    count = y - x
+    result = int(time.time()) % count
+    return result + x
+print(getrand(100, 80))
